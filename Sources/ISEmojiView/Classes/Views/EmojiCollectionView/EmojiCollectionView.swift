1279920ab9f3981807f50845c004811bd7c3b90b
Michal Smialko <michal.smialko@gmail.com>
2021-06-16 10:25:50 +0200

    SPM support.

--- Sources/ISEmojiView/Classes/Views/EmojiCollectionView/EmojiCollectionView.swift
+++ Sources/ISEmojiView/Classes/Views/EmojiCollectionView/EmojiCollectionView.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 /// emoji view action callback delegate
 internal protocol EmojiCollectionViewDelegate: class {
--- Sources/ISEmojiView/Classes/Views/EmojiView/EmojiView.swift
+++ Sources/ISEmojiView/Classes/Views/EmojiView/EmojiView.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public enum BottomType: Int {
     case pageControl, categories
