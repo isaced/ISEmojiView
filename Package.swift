bb3be72997e48dd229e81d1bb1040c5dd15a3356
Zsolt Váradi <vzsg@users.noreply.github.com>
2021-07-07 14:41:24 +0200

    Reduce iOS deployment target to 12.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ISEmojiView",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
